e4ca2a60c6d2a04b2d48579e8dbef70595f35393
Rafed Taieb <rafedtaieb44@gmail.com>
2026-07-25 14:00:32 +0200

    Added SERVICES without DEPENDICES INJECTION

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,72 +2,36 @@ import { Controller,Get,Post,Body,Param,NotFoundException,Put, Delete,ParseIntPi
 
 import { CreateProductDto } from "./dto/create-product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
+import { ProductsService } from "./products.service";
 
-type ProductType = {
-    id: number;
-    name: string;
-    price: number;
-};
+
 @Controller("/api/products")
 export class ProductsController {
-    private products: ProductType[] = [
-        {id:1, name: "Product 1", price: 10.99},
-        {id:2, name: "Product 2", price: 19.99},
-        {id:3, name: "Product 3", price: 5.99},
-    ];
 
-
-
-
+    //this consider a bad practice method i will fix it later by using dependency injection
+    private productsService: ProductsService = new ProductsService();
+   
 
     @Post()
     public createNewProduct(@Body(new ValidationPipe()) body:CreateProductDto) {
-        const newProduct: ProductType = {
-            id: this.products.length + 1,
-            name: body.name,
-            price: body.price
-        };
-        this.products.push(newProduct);
-        return newProduct;
+        return this.productsService.createProduct(body);
+
     }
     @Get()
     public getAllProducts() {
-        return this.products;
+        return this.productsService.getAll();
     }
     @Get(":id")
     public getProductById(@Param("id", ParseIntPipe) id: number) {
-        console.log(typeof id);
-        
-        const product =this.products.find(p=> p.id === (id));
-        if(!product) {
-            throw new NotFoundException(`Product with id ${id} not found`);
-        }else {
-        return product;
-        }
+        return this.productsService.getOneBy(id);
     }
     @Put(":id")
     public updateProductById(@Param("id", ParseIntPipe) id: number, @Body(new ValidationPipe()) body:UpdateProductDto) {
-        console.log(body);
-        const productIndex = this.products.findIndex(p=> p.id === (id));
-        if(productIndex === -1) {
-            throw new NotFoundException(`Product with id ${id} not found`);
-        }
-        const updatedProduct = {
-            ...this.products[productIndex],
-            ...body
-        };
-        this.products[productIndex] = updatedProduct;
-        return updatedProduct;
+        return this.productsService.update(id, body);
     }
     @Delete(":id")
     public deleteProductById(@Param("id", ParseIntPipe) id: number) {
-        const productIndex = this.products.findIndex(p=> p.id === (id));
-        if(productIndex === -1) {
-            throw new NotFoundException(`Product with id ${id} not found`);
-        }
-        const deletedProduct = this.products[productIndex];
-        this.products.splice(productIndex, 1);
-        return deletedProduct;
+        return this.productsService.delete(id);
     }
         
 
---
+++ src/products/products.service.ts
@@ -0,0 +1,93 @@
+import { NotFoundException } from "@nestjs/common";
+import { CreateProductDto } from "./dto/create-product.dto";
+import { UpdateProductDto } from "./dto/update-product.dto";
+
+type ProductType = {
+    id: number;
+    name: string;
+    price: number;
+};
+export class ProductsService {
+    private products: ProductType[] = [
+        {id:1, name: "Product 1", price: 10.99},
+        {id:2, name: "Product 2", price: 19.99},
+        {id:3, name: "Product 3", price: 5.99},
+    ];
+
+        /**
+         * create new product
+         */
+        public createProduct({name, price}:CreateProductDto) {
+            const newProduct: ProductType = {
+                id: this.products.length + 1,
+                name,
+                price
+            };
+            this.products.push(newProduct);
+            return newProduct;
+        }
+
+
+        /**
+         * get all products
+         */
+       
+        public getAll() {
+            return this.products;
+        }
+
+        /**
+         * get one product by id
+         */
+        
+        public getOneBy(id: number) {
+            console.log(typeof id);
+            
+            const product =this.products.find(p=> p.id === (id));
+            if(!product) {
+                throw new NotFoundException(`Product with id ${id} not found`);
+            }else {
+            return product;
+            }
+        }
+
+
+        /**
+         * Update product by id
+         */
+        
+        public update(id: number, UpdateProductDto:UpdateProductDto) {
+            
+            const productIndex = this.products.findIndex(p=> p.id === (id));
+            if(productIndex === -1) {
+                throw new NotFoundException(`Product with id ${id} not found`);
+            }
+            const updatedProduct = {
+                ...this.products[productIndex],
+                ...UpdateProductDto
+            };
+            this.products[productIndex] = updatedProduct;
+            return updatedProduct;
+        }
+
+
+
+
+
+        /**
+         * Delete product by id
+         */
+
+        
+        
+        public delete( id: number) {
+            const productIndex = this.products.findIndex(p=> p.id === (id));
+            if(productIndex === -1) {
+                throw new NotFoundException(`Product with id ${id} not found`);
+            }
+            const deletedProduct = this.products[productIndex];
+            this.products.splice(productIndex, 1);
+            return deletedProduct;
+        }
+            
+}
