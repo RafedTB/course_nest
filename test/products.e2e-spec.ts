ae20211c5a3bb4a22a1d7d9cf646ce7eb72f4587
Rafed Taieb <rafedtaieb44@gmail.com>
2026-08-22 18:39:02 +0200

    Starting E2E Test

---
+++ test/products.e2e-spec.ts
@@ -0,0 +1,81 @@
+import {Test,TestingModule} from '@nestjs/testing';
+import {DataSource} from 'typeorm';
+import {INestApplication} from '@nestjs/common';
+import request from 'supertest';
+import {AppModule} from '../src/app.module';
+import {Product} from "../src/products/product.entity";
+import { CreateProductDto } from 'src/products/dto/create-product.dto';
+
+describe('ProductsController (e2e)', () => {
+    let app: INestApplication;
+    let dataSource: DataSource;
+    let productToSave: CreateProductDto[];
+    beforeAll(async () => {
+        productToSave = [
+            {
+                name: "Product 1",
+                description: "Description for Product 1",
+                price: 10.99
+            },
+            {
+                name: "Product 2",
+                description: "Description for Product 2",
+                price: 20.99
+            },
+            {
+                name: "Product 3",
+                description: "Description for Product 3",
+                price: 30.99
+            },
+            {
+                name: "Product 4",
+                description: "Description for Product 4",
+                price: 40.99
+            }
+        ];
+        const module:TestingModule = await Test.createTestingModule({
+            imports: [AppModule]
+        }).compile();
+
+        app=module.createNestApplication();
+        await app.init();
+        dataSource=app.get(DataSource);
+    });
+    afterEach(async () => {
+    await dataSource
+        .createQueryBuilder()
+        .delete()
+        .from(Product)
+        .execute();
+});
+
+afterAll(async () => {
+    await app.close();
+});
+    //get api/products
+    describe('GET /api/products', () => {
+        it('should return all products', async () => {
+            // Save products to the database
+            await dataSource.createQueryBuilder().insert().into(Product).values(productToSave).execute();
+            const response = await request(app.getHttpServer()).get('/api/products');
+            expect(response.status).toBe(200);
+            expect(response.body.length).toBe(productToSave.length);
+
+        })
+        it("should return products based on name",async()=>{
+            await dataSource.createQueryBuilder().insert().into(Product).values(productToSave).execute();
+            const response = await request(app.getHttpServer()).get('/api/products?name=Product 1');
+            expect(response.status).toBe(200);
+            expect(response.body.length).toBe(1);
+            expect(response.body[0].name).toBe("Product 1");
+        })
+        it("should return products based on minPrice and maxPrice",async()=>{
+            await dataSource.createQueryBuilder().insert().into(Product).values(productToSave).execute();
+            const response = await request(app.getHttpServer()).get('/api/products?minPrice=15&maxPrice=35');
+            expect(response.status).toBe(200);
+            expect(response.body.length).toBe(2);
+            expect(response.body[0].name).toBe("Product 3");
+            expect(response.body[1].name).toBe("Product 2");
+        })
+    })
+});
